83735c093c34bd74e4a86f0559e6bede4f64b776
MartinPiloni <maartinpiloni@gmail.com>
2023-01-02 22:59:18 -0300

    CPP template for USACO

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void setIO(string name = "") {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+	if ((int) name.size()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+int main() {
+	setIO();
+
+    return 0;
+}
