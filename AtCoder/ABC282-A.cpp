584c5f3c8ef05a917172656b5613cdd94db2f808
MartinPiloni <maartinpiloni@gmail.com>
2023-01-05 16:21:27 -0300

    AtCoder/ABC282-A

---
+++ AtCoder/ABC282-A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using str = string;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int k;
+	cin >> k;
+
+	for (int i = 0; i < k; i++) {
+		cout << (char)(i + 65);
+	}
+
+	cout << "\n";
+    return 0;
+}
