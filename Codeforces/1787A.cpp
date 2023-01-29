be4e3669c86c2abfcff316e2c29e319aaf576321
MartinPiloni <maartinpiloni@gmail.com>
2023-01-29 14:32:33 -0300

    Codeforces 1787A

---
+++ Codeforces/1787A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using str = string;
+
+#define sz(x) (int) (x).size()
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+
+		if (n % 2 == 0) {
+			cout << 1 << " " << n / 2 << "\n";
+		} else {
+			cout << -1 << "\n";
+		}
+	}
+
+    return 0;
+}
