cc6241c578a4158966b15fed3911fe30a01f608b
MartinPiloni <maartinpiloni@gmail.com>
2023-01-30 21:10:24 -0300

    Project Euler Problem 9

--- ProjectEuler/P009.cpp
+++ ProjectEuler/P009.cpp
@@ -11,8 +11,8 @@ int main() {
 	cin.tie(nullptr);
 
 	for (int i = 1; i < 1000; i++) {
-		for (int j = i + 1; j < 1000; j++) {
-			for (int k = j + 1; k < 1000; k++) {
+		for (int j = i; j <= 1000 - i; j++) {
+			for (int k = j; k <= 1000 - i - j; k++) {
 				if ((i * i + j * j == k * k) && (i + j + k == 1000)) {
 					cout << i * j * k << "\n";
 					return 0;
