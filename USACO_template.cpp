a333fd74c99b66bf160a4246f5d80f4e744f8737
MartinPiloni <maartinpiloni@gmail.com>
2023-04-07 12:02:23 -0300

    fix indentation

--- USACO_template.cpp
+++ USACO_template.cpp
@@ -4,19 +4,17 @@ using namespace std;
 using ll = long long;
 using str = string;
 
-#define sz(x) (int) (x).size()
-
 void setIO(string name = "") {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-	if ((int) name.size()) {
-		freopen((name + ".in").c_str(), "r", stdin);
-		freopen((name + ".out").c_str(), "w", stdout);
-	}
+    if ((int) name.size()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
 }
 
 int main() {
-	setIO();
+    setIO();
 
     return 0;
 }
