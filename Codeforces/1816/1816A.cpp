a3a37901e1973ee76bdd611544ee6f63f083fb52
MartinPiloni <maartinpiloni@gmail.com>
2023-04-11 23:31:06 -0300

    Codeforces 1816A

---
+++ Codeforces/1816/1816A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using str = string;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a, b;
+        cin >> a >> b;
+        cout << "2\n" << a - 1 << " 1\n" << a << " " << b << "\n";
+    }
+
+    return 0;
+}
