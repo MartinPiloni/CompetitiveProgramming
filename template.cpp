65ccf8f2a6b58ea32062232862334e43f6c42e83
MartinPiloni <maartinpiloni@gmail.com>
2023-07-01 21:30:29 -0300

    Template update

--- template.cpp
+++ template.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(int i=a,ggdem=b;i<ggdem;++i)
+#define SZ(x) ((int)x.size())
+#define ALL(x) x.begin(),x.end()
+#define mset(a,v) memset((a),(v),sizeof(a))
+
 using ll = long long;
 using str = string;
-
-#define sz(x) int((x).size())
 
 int main() {
     ios::sync_with_stdio(false);
