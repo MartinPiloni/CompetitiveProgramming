587b37450e57577106119215803bae226471820f
MartinPiloni <maartinpiloni@gmail.com>
2023-07-09 20:54:08 -0300

    Codeforces 1807A

---
+++ Codeforces/1807/1807A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fst first
+#define snd second
+#define sz(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+
+using ll = long long;
+using str = string;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a + b == c) {
+            cout << "+\n";
+        } else {
+            cout << "-\n";
+        }
+    }
+
+    return 0;
+}
