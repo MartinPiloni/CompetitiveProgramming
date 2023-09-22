9f870cb4723b3c6791de78e26ff3eb18b7f989f1
MartinPiloni <maartinpiloni@gmail.com>
2023-09-22 16:27:41 -0300

    ICPC Regional LATAM 2022-2023 E

--- ICPC/2022-2023_LATAM_REGIONAL/e.cpp
+++ ICPC/2022-2023_LATAM_REGIONAL/e.cpp
@@ -7,15 +7,14 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N, K, E;
-    cin >> N >> K >> E;
+    int n, k, e;
+    cin >> n >> k >> e;
 
     multiset<int> s;
-    s.insert(E);
-    s.insert(K);
-    s.insert(N - (K + E));
+    s.insert(e);
+    s.insert(k);
+    s.insert(n - (k + e));
 
-    auto it = s.begin();
     if (s.count(1) == 3 || s.count(4) == 3) {
         cout << "2\n";
     } else if (s.count(2) == 3 || s.count(3) == 3){
@@ -23,14 +22,14 @@ int main() {
     } else if (s.count(1) == 2) {
         cout << "1\n";
     } else if (s.count(2) == 2) {
-        it = s.upper_bound(2);
+        auto it = s.upper_bound(2);
         if (it != s.end() || s.find(0) != s.end()) {
             cout << "1\n";
         } else {
             cout << "2\n";
         }
     } else if (s.count(3) == 2) {
-        it = s.lower_bound(3);
+        auto it = s.lower_bound(3);
         if (it != s.begin()) {
             it--;
             cout << *it << "\n";
@@ -38,7 +37,7 @@ int main() {
             cout << "0\n";
         }
     } else if (s.count(4) == 2) {
-        it = s.lower_bound(4);
+        auto it = s.lower_bound(4);
         if (it != s.begin()) {
             it--;
             cout << *it % 2 << "\n";
