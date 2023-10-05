215b048824a6452904ad48468ae8e56a8a961a08
MartinPiloni <maartinpiloni@gmail.com>
2023-10-05 00:51:20 -0300

    ICPC Brasil SubRegional I

--- ICPC/2023-2024_Brazil_SUBREGIONAL/I.cpp
+++ ICPC/2023-2024_Brazil_SUBREGIONAL/I.cpp
@@ -10,9 +10,42 @@ using namespace std;
 using ll = long long;
 using str = string;
 
+const int MAXN = 1e5 + 10;
+ll dp[MAXN][2];
+int n;
+int v[MAXN];
+
+ll f(int i, bool p) {
+    if (i == n) return 0;
+
+    ll &r = dp[i][p];
+    if (r != -1) return r;
+
+    if (p && v[i] == 0) {
+        r = 1 + f(i + 1, p);
+    } else if (v[i] == 0) {
+        r = f(i + 1, p);
+    } else if (!p && v[i] == 1) {
+        r = 1 + f(i + 1, !p);
+    } else {
+        r = f(i + 1, !p);
+    } 
+
+    return r;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    memset(dp, -1, sizeof dp);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> v[i];
+    
+    ll ans = 0;
+    for (int i = 0; i < n; i++) ans += f(i, 0);
+    cout << ans << "\n";
 
     return 0;
 }
